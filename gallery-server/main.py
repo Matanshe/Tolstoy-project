191167f1ab0dbb7b8601463bffed7be566665066
matan shemesh <matan.shemesh@zoominsoftware.com>
2023-02-12 20:27:31 +0200

    server upload image

--- gallery-server/main.py
+++ gallery-server/main.py
@@ -1,8 +1,9 @@
 import logging
+import os
 import sqlite3
 import sys
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -12,7 +13,24 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(mes
 
 @app.route('/api/upload', methods=['POST'])
 def upload_image():
-    pass
+    image_file = request.files['image']
+    name = request.form['name']
+    date = request.form['date']
+    description = request.form['description']
+
+    image_path = os.path.join('images', image_file.filename)
+    image_file.save(image_path)
+
+    conn = sqlite3.connect('images.db')
+    c = conn.cursor()
+
+    c.execute("INSERT INTO images (name, date, description, path) VALUES (?,?,?,?)",
+              (name, date, description, image_path))
+    conn.commit()
+    conn.close()
+    logger.info(f"Image uploaded successfully: name={name}, date={date}, description={description}")
+
+    return jsonify({'message': 'Image uploaded successfully'})
 
 
 @app.route('/api/images', methods=['GET'])
